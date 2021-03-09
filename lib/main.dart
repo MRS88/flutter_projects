458afa0e6da5c01749fa547acb4d9a37a9be15cd
Lebovski <salavat.mr@gmail.com>
2021-03-09 21:45:09 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyBody extends StatelessWidget{
   Widget build(BuildContext context) {
+    var textButton = new TextButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red[100]),),
+        onPressed: () async {
+          const url = 'https://flutter.su/';
+          if (await canLaunch(url)) {
+            await launch(url);
+          } else {
+            throw 'Could not launch $url';
+          }
+        },
+        child: Text('open link'),);
     return new Center(child: new Column(
       children: [
-        new Text('Hello, guys!'),
-        new TextButton(style: ButtonStyle(foregroundColor: MaterialStateProperty.all(Colors.red),
-        ),
-        onPressed: (){},
-        child: Text('open link'),)
+        new Text('Hello, guys!'), textButton
       ]));
   }
 }
@@ -18,8 +25,9 @@ void main() {
     new MaterialApp(
       debugShowCheckedModeBanner: false,
       home: new Scaffold(
-        appBar: new AppBar(title: new Text('MyFlutter.su')),
-        body: new MyBody()
+        appBar: new AppBar(title: new Text('Flutter.su')),
+        body: new MyBody(),
+        backgroundColor: Colors.grey
           )
         )
       );
