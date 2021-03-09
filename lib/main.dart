14fefbf135819e534479a34a4a0a9156596b611b
Lebovski <salavat.mr@gmail.com>
2021-03-09 21:01:41 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+
+class MyBody extends StatelessWidget{
+  Widget build(BuildContext context) {
+    return new Center(child: new Column(
+      children: [
+        new Text('Hello, guys!'),
+        new TextButton(style: ButtonStyle(foregroundColor: MaterialStateProperty.all(Colors.red),
+        ),
+        onPressed: (){},
+        child: Text('open link'),)
+      ]));
+  }
+}
+
 void main() {
   runApp(
     new MaterialApp(
       debugShowCheckedModeBanner: false,
       home: new Scaffold(
         appBar: new AppBar(title: new Text('MyFlutter.su')),
-        body: new Center(
-          child: new Column(
-            children: [new Text('Hello, World!'), new TextButton(onPressed: (){}, child: Text('open site'))]
+        body: new MyBody()
           )
         )
-      ) 
-    )
-  );
+      );
 }
